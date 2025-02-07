752991ad2a1ce15a1217aa0bfdec3d4b0c554dd6
niickcarva <nicolasc.avelaneda@gmail.com>
2025-02-07 17:32:26 -0300

    chore: improvement on api architecture using solid

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,25 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./http/routes";
+import { ZodError } from "zod";
+import { env } from "./env";
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
-prisma.user.create({
-  data: {
-    name: "John",
-    email: "john@doe.com",
-  },
+app.register(appRoutes);
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "Validation error.", issues: error.format() });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  } else {
+    // TODO: handle production errors
+    // external tool like sentry/datadog/NewRelic
+  }
+
+  return reply.status(500).send({ message: "Internal server error." });
 });
