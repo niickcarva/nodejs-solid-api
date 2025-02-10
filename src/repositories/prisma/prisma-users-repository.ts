a25d5089c60bbdfbbc99dea6d51f648f29ccb0d1
niickcarva <nicolasc.avelaneda@gmail.com>
2025-02-10 11:56:35 -0300

    feat: implement prisma check in repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,9 +1,19 @@
 import { prisma } from "@/lib/prisma";
-import { Prisma, User } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 import { UsersRepository } from "../users-repository";
 
 export class PrismaUsersRepository implements UsersRepository {
-  async findByEmail(email: string): Promise<User | null> {
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return user;
+  }
+
+  async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
         email,
